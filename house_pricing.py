4d9b94c3b3cb207c73cf897aae9bc380cec78cfb
SaviruDeSilva <savirurandika25@gmail.com>
2026-01-10 16:05:29 +0530

    Update house_pricing.py

--- house_pricing.py
+++ house_pricing.py
@@ -22,7 +22,7 @@ st.write("This app uses **KMeans clustering + Random Forest regression**")
 # -----------------------------
 @st.cache_data
 def load_data():
-    return pd.read_csv("housepricing/housing.csv")
+    return pd.read_csv("housing.csv")
 
 df = load_data()
 
